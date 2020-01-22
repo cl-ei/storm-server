5646d981cadfad29404748d87e10fae771f4da40
Caoliang <i@caoliang.net>
2020-01-22 16:14:03 +0800

    Sep.

--- web/handlers/bili.py
+++ web/handlers/bili.py
@@ -304,4 +304,9 @@ async def user_info(request):
     user_obj = await BiliUser.get_by_uid_or_name(user_str)
     if not user_obj:
         return web.Response(text=f"未收录该用户: {user_str}", content_type="text/html")
-    return web.Response(text=f"用户: {user_obj.name}(uid: {user_obj.uid})", content_type="text/html")
+
+    context = {
+        "user_name": user_obj.name,
+        "uid": user_obj.uid
+    }
+    return render_to_response(template="web/templates/user_info", context=context)
